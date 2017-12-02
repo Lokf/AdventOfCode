0c62ee56b13ed283544042bdef12ff06ec10757d
Lokf <wistrom.fredrik@gmail.com>
2017-12-02 13:09:55 +0100

    Day 2.

---
+++ AdventOfCode/Test/Day2Test.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Test
+{
+    [TestClass]
+    public class Day2Test
+    {
+        [TestMethod]
+        public void Task1()
+        {
+            var spreadsheat = new List<string>
+            {
+                "5 1 9 5",
+                "7 5 3",
+                "2 4 6 8"
+            };
+
+            Assert.AreEqual(18, Day2.Task1(spreadsheat));
+        }
+
+        [TestMethod]
+        public void Task2()
+        {
+            var spreadsheat = new List<string>
+            {
+                "5 9 2 8",
+                "9 4 7 3",
+                "3 8 6 5"
+            };
+
+            Assert.AreEqual(9, Day2.Task2(spreadsheat));
+        }
+    }
+}
