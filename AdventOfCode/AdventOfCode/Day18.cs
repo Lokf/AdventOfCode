cdc000085a1d5a383f5e9c3c2aa72d56e6ba0520
Lokf <wistrom.fredrik@gmail.com>
2018-01-02 20:48:33 +0100

    Refactoring day 18.

--- AdventOfCode/AdventOfCode/Day18.cs
+++ AdventOfCode/AdventOfCode/Day18.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode
@@ -53,22 +54,6 @@ namespace AdventOfCode
             }
         }
 
-        public static int Task2(string[] instructions)
-        {
-            var program0 = new Program(0, instructions);
-            var program1 = new Program(1, instructions);
-            
-            var send0 = new Queue<long>();
-            var send1 = new Queue<long>();
-
-            while (program0.ExecuteNextInstruction(send0, send1) || program1.ExecuteNextInstruction(send1, send0))
-            {
-                ;
-            }
-
-            return program1.SendCount;
-        }
-
         private static long GetValue(Dictionary<char, long> dictionary, char register)
         {
             if (dictionary.TryGetValue(register, out var value))
@@ -100,21 +85,43 @@ namespace AdventOfCode
             dictionary.Add(key, value);
         }
 
+        public static int Task2(string[] instructions)
+        {
+            var send0 = new Queue<long>();
+            var send1 = new Queue<long>();
+
+            var program0 = new Program(0, instructions, send0, send1);
+            var program1 = new Program(1, instructions, send1 ,send0);                      
+
+            while (program0.ExecuteNextInstruction() || program1.ExecuteNextInstruction())
+            {
+                ;
+            }
+
+            return program1.SendCount;
+        }
+
         private class Program
         {
             private readonly Dictionary<char, long> registers = new Dictionary<char, long>();
-            private readonly string[] instructions;
+            private readonly IInstruction[] instructions;
+            private readonly Queue<long> send;
+            private readonly Queue<long> receive;
             private long index = 0;
 
             public int SendCount { get; private set; } = 0;
 
-            public Program(long programId, string[] instructions)
+            public Program(long programId, string[] instructions, Queue<long> send, Queue<long> receive)
             {
+                this.instructions = ParseInstructions(instructions);
+                this.send = send;
+                this.receive = receive;
+
                 var registerKeys = instructions
                     .Select(x => x.Split(' '))
-                    .ToArray()[1]
+                    .Select(x => x[1][0])
                     .Distinct()
-                    .Select(x => x[0]);
+                    .Where(x => x != '\0' && !char.IsDigit(x));
 
                 foreach (var key in registerKeys)
                 {
@@ -122,66 +129,243 @@ namespace AdventOfCode
                 }
 
                 registers['p'] = programId;
-
-                this.instructions = instructions; 
             }
 
-            public bool ExecuteNextInstruction(Queue<long> send, Queue<long> receive)
+            public bool ExecuteNextInstruction()
             {
                 if (index < 0 || index >= instructions.Length)
                 {
                     return false;
                 }
 
-                var instruction = instructions[index]
-                    .Split(' ')
-                    .ToArray();
+                var instruction = instructions[index];
+                var success = ExecuteInstruction(instruction);
 
-                switch (instruction[0])
+                if (success)
                 {
-                    case "snd":
-                        send.Enqueue(GetValue(instruction[1]));
-                        SendCount++;
-                        break;
-                    case "set":
-                        registers[instruction[1][0]] = GetValue(instruction[2]);
-                        break;
-                    case "add":
-                        registers[instruction[1][0]] += GetValue(instruction[2]);
-                        break;
-                    case "mul":
-                        registers[instruction[1][0]] *= GetValue(instruction[2]);
-                        break;
-                    case "mod":
-                        registers[instruction[1][0]] %= GetValue(instruction[2]);
-                        break;
-                    case "rcv":
-                        if (receive.Count == 0)
-                        {
-                            return false;
-                        }
-                        registers[instruction[1][0]] = receive.Dequeue();
-                        break;
-                    case "jgz":
-                        if (GetValue(instruction[1]) > 0)
-                        {
-                            index += GetValue(instruction[2]) - 1;
-                        }
-                        break;
+                    index++;
                 }
 
-                index++;
+                return success;
+            }
+
+            private bool ExecuteInstruction(IInstruction instruction)
+            {
+                switch (instruction)
+                {
+                    case SendInstruction send:
+                        return ExecuteInstruction(send);
+                    case ReceiveInstruction receive:
+                        return ExecuteInstruction(receive);
+                    case SetInstruction set:
+                        return ExecuteInstruction(set);
+                    case AddInstruction add:
+                        return ExecuteInstruction(add);
+                    case MultiplyInstruction multiply:
+                        return ExecuteInstruction(multiply);
+                    case ModulusInstruction modulus:
+                        return ExecuteInstruction(modulus);
+                    case JumpInstruction jump:
+                        return ExecuteInstruction(jump);
+                    default:
+                        throw new InvalidOperationException($"Unknown instruction: {instruction.GetType()}.");
+                }
+            }
+
+            private bool ExecuteInstruction(SendInstruction instruction) {
+                send.Enqueue(instruction.Value);
+                SendCount++;
                 return true;
             }
 
-            private long GetValue(string value)
+            private bool ExecuteInstruction(ReceiveInstruction instruction)
             {
-                if (long.TryParse(value, out var kalle))
+                if (receive.Count == 0)
                 {
-                    return kalle;
+                    return false;
                 }
 
-                return registers[value[0]];
+                registers[instruction.RegisterAddress] = receive.Dequeue();
+                return true;
+            }
+
+            private bool ExecuteInstruction(SetInstruction instruction)
+            {
+                registers[instruction.RegisterAddress] = instruction.Value;
+                return true;
+            }
+
+            private bool ExecuteInstruction(AddInstruction instruction)
+            {
+                registers[instruction.RegisterAddress] += instruction.Value;
+                return true;
+            }
+
+            private bool ExecuteInstruction(MultiplyInstruction instruction)
+            {
+                registers[instruction.RegisterAddress] *= instruction.Value;
+                return true;
+            }
+
+            private bool ExecuteInstruction(ModulusInstruction instruction)
+            {
+                registers[instruction.RegisterAddress] %= instruction.Value;
+                return true;
+            }
+
+            private bool ExecuteInstruction(JumpInstruction instruction)
+            {
+                if (instruction.CompareValue > 0)
+                {
+                    index += instruction.JumpValue - 1;
+                }
+                return true;
+            }
+
+            private IInstruction[] ParseInstructions(string[] instructions)
+            {
+                return instructions
+                    .Select(ParseInstruction)
+                    .ToArray();
+            }
+
+            private IInstruction ParseInstruction(string instruction)
+            {
+                var parts = instruction
+                        .Split(' ')
+                        .ToArray();
+
+                switch (parts[0])
+                {
+                    case "snd":
+                        return new SendInstruction(GetValue(parts[1]));
+                    case "rcv":
+                        return new ReceiveInstruction(parts[1][0]);
+                    case "set":
+                        return new SetInstruction(parts[1][0], GetValue(parts[2]));
+                    case "add":
+                        return new AddInstruction(parts[1][0], GetValue(parts[2]));
+                    case "mul":
+                        return new MultiplyInstruction(parts[1][0], GetValue(parts[2]));
+                    case "mod":
+                        return new ModulusInstruction(parts[1][0], GetValue(parts[2]));
+                    case "jgz":
+                        return new JumpInstruction(GetValue(parts[1]), GetValue(parts[2]));
+                    default:
+                        throw new ArgumentException($"Unknown instruction: {parts[0]}.", nameof(instruction));
+                }
+            }
+
+            private IValue GetValue(string value)
+            {
+                if (long.TryParse(value, out var a))
+                {
+                    return new ScalarValue(a);
+                }
+
+                return new RegisterValue(value[0], registers);
+            }
+        }        
+
+        private interface IValue
+        {
+            long Value { get; }
+        }
+
+        private class ScalarValue : IValue
+        {
+            public long Value { get; }
+
+            public ScalarValue(long value)
+            {
+                Value = value;
+            }
+        }
+
+        private class RegisterValue : IValue
+        {
+            private readonly char key;
+            private readonly Dictionary<char, long> registers;
+            public long Value => registers[key];
+
+            public RegisterValue(char key, Dictionary<char, long> registers)
+            {
+                this.key = key;
+                this.registers = registers;
+            }
+        }
+
+        private interface IInstruction { }
+
+        private class SendInstruction : IInstruction
+        {
+            private readonly IValue value;
+            public long Value => value.Value;
+
+            public SendInstruction(IValue value)
+            {
+                this.value = value;
+            }
+        }
+
+        private class ReceiveInstruction : IInstruction
+        {
+            public char RegisterAddress { get; }
+
+            public ReceiveInstruction(char registerAddress)
+            {
+                RegisterAddress = registerAddress;
+            }
+        }
+
+        private abstract class RegisterAddressAndValueInstruction : IInstruction
+        {
+            private readonly IValue value;
+            public long Value => value.Value;
+            public char RegisterAddress { get; }
+
+            public RegisterAddressAndValueInstruction(char registerAddress, IValue value)
+            {
+                RegisterAddress = registerAddress;
+                this.value = value;
+            }
+        }
+
+        private class SetInstruction : RegisterAddressAndValueInstruction
+        {
+            public SetInstruction(char registerAddress, IValue value) 
+                : base(registerAddress, value) { }
+        }
+
+        private class AddInstruction : RegisterAddressAndValueInstruction
+        {
+            public AddInstruction(char registerAddress, IValue value)
+               : base(registerAddress, value) { }
+        }
+
+        private class MultiplyInstruction : RegisterAddressAndValueInstruction
+        {
+            public MultiplyInstruction(char registerAddress, IValue value)
+               : base(registerAddress, value) { }
+        }
+
+        private class ModulusInstruction : RegisterAddressAndValueInstruction
+        {
+            public ModulusInstruction(char registerAddress, IValue value)
+               : base(registerAddress, value) { }
+        }
+
+        private class JumpInstruction : IInstruction
+        {
+            private readonly IValue compareValue;
+            private readonly IValue jumpValue;
+            public long CompareValue => compareValue.Value;
+            public long JumpValue => jumpValue.Value;
+
+            public JumpInstruction(IValue compareValue, IValue jumpValue)
+            {
+                this.compareValue = compareValue;
+                this.jumpValue = jumpValue;
             }
         }
     }
