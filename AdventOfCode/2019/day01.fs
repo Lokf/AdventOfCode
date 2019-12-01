12214024a5baa6cccd857c02698364494f6a1cdd
Fredrik Wiström <Fredrik.Wistrom@soderbergpartners.se>
2019-12-01 17:39:42 +0100

    Solve day 01 puzzle 1

---
+++ AdventOfCode/2019/day01.fs
@@ -0,0 +1,12 @@
+﻿namespace _2019
+
+module Day01 =
+    let fuelRequired mass =
+        (mass / 3) - 2
+
+    let path = System.IO.Path.Combine(__SOURCE_DIRECTORY__, "day01.txt")
+    let masses = System.IO.File.ReadAllLines(path) |> Array.toList
+    let puzzle1 =
+        List.map int masses
+        |> List.map(fun(x) -> fuelRequired x)
+        |> List.sum
