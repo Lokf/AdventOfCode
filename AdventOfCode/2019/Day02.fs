acedb03e4e31e0fa62f11b97c3c9ad549e7377a2
Fredrik Wiström <Fredrik.Wistrom@soderbergpartners.se>
2019-12-03 07:45:40 +0100

    Solve day 02 puzzle 2

--- AdventOfCode/2019/Day02.fs
+++ AdventOfCode/2019/Day02.fs
@@ -18,8 +18,26 @@ module Day02 =
             program.[0]
 
     let path = System.IO.Path.Combine(__SOURCE_DIRECTORY__, "day02.txt")
-    let program = Input. readIntsFromCsv path
+    let input = Input.readIntsFromCsv path
+                  |> List.toArray
 
     let puzzle1 = 
+        let program = Array.copy input
+        Array.set program 1 12
+        Array.set program 2 2
+        exec program 0
 
+    let rec loop input noun verb =
+        let program = Array.copy input
+        Array.set program 1 noun
+        Array.set program 2 verb
+        let res = exec program 0
+        if res = 19690720
+        then 100 * noun + verb
+        else if verb = 99
+        then loop input (noun + 1) 0
+        else loop input noun (verb + 1)
+
+    let puzzle2 =
+        loop input 0 0
 
